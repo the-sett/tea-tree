f2ff8a63654f921431d01aabfab2b992845a812d
Rupert Smith <rupert@thesett.com>
2018-07-03 16:39:19 +0100

    Pulling together packages for the example.

---
+++ example/Main.elm
@@ -0,0 +1,19 @@
+module Main exposing (..)
+
+import AnimationFrame
+import Arc2d
+import Ease
+import Geometry.Svg
+import Ports.SVGTextPort exposing (textToSVG, textToSVGResponse)
+import TeaTree
+import TextToSVG exposing (textAsPath, textAsText, TextAlignment(..), TextRenderFunc)
+
+
+type Model
+    = SizingText
+    | Ready
+
+
+type Msg
+    = TextToSVGMsg TextToSVG.Msg
+    | ClickElement String
