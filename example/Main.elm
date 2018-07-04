505e0466f60adcb75a05dfd42d3a0196f0e9f250
Rupert Smith <rupert@thesett.com>
2018-07-04 16:42:43 +0100

    Getting the node types right.

--- example/Main.elm
+++ example/Main.elm
@@ -2,9 +2,11 @@ module Main exposing (..)
 
 import AnimationFrame
 import Arc2d
+import Color exposing (Color)
 import Ease
 import Geometry.Svg
 import Http
+import Json.Decode
 import Ports.SVGTextPort exposing (textToSVG, textToSVGResponse)
 import TeaTree exposing (Tree)
 import TextToSVG exposing (textAsPath, textAsText, TextAlignment(..), TextRenderFunc)
@@ -21,13 +23,30 @@ type Msg
     | ClickElement String
 
 
-type alias Example =
+type alias Wedge =
     { label : String
     , size : Float
+    , startAngle : Float
+    , endAngle : Float
+    , innerRadius : Float
+    , outerRadius : Float
+    , color : Color
     }
 
 
-example : Tree Example
+
+--decoder : Decoder (Tree Wedge)
+
+
+example : Tree Wedge
 example =
-    TeaTree.singleton { label = "", size = 0.0 }
+    TeaTree.singleton
+        { label = ""
+        , size = 0.0
+        , startAngle = 0.0
+        , endAngle = 0.0
+        , innerRadius = 0.0
+        , outerRadius = 0.0
+        , color = Color.rgb 0 0 0
+        }
         |> TeaTree.toTree
